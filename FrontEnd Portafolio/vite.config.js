ddaed6ffbbe188c023f8157ccadfa38e1cf36002
Rob <arq.robertoriv@gmail.com>
2026-06-09 19:28:26 -0600

    chore: setup github pages deploy and remove backend

--- FrontEnd Portafolio/vite.config.js
+++ FrontEnd Portafolio/vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/robrivmx-portfolio/', // <-- Esta es la línea maestra para GitHub Pages
 });
